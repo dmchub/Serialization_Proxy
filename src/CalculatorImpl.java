11e9d22167452c0f1c416afdfe90f2773cf93f17
dmchub <37676729+dmchub@users.noreply.github.com>
2020-06-30 10:01:17 +0300

    Initial commit: Implementation for standard calculator

---
+++ src/CalculatorImpl.java
@@ -0,0 +1,8 @@
+public class CalculatorImpl implements Calculator {
+    @Override
+    public int calc(int arg){
+        int result = arg * arg;
+        System.out.println("Result: " + result + " is calculated");
+        return result;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new CalculatorImpl();
+
+        calculator.calc(1);
+        calculator.calc(1);
+    }
+}
